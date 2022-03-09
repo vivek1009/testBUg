e01e85d50d966608aeeed5153b115952a2ff5e20
VIVEK HALDIYA <vivekhaldiya@gmail.com>
2022-03-09 15:54:54 +0530

    Add files via upload

---
+++ default_vcn/variables.tf
@@ -0,0 +1,27 @@
+
+    // Copyright (c) 2020, Oracle and/or its affiliates. All rights reserved.
+    // Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+    
+    variable "compartment_ocid" {}
+    variable "tenancy_ocid" {}
+    variable "region" {}
+
+    variable "vcn_display_name" {
+      type = "string"
+      default = "1234"
+    }
+    
+    variable "vcn_cidr" {
+      type = "string"
+      default = " 10-04-19 12:00:17"
+    }
+
+    variable "vcn_dns_label" {
+      default     = "vcn"
+    }
+
+    variable "subnet_dns_label" {
+      default = "subnet"
+    }
+
+  
